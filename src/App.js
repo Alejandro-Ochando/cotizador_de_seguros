59b7295b2563bbbaec426b7d369ab02b4abb302d
Alejandro-Ochando <alejandro.ochando@live.com>
2020-09-27 20:21:20 +0200

    Instalacion Emotion: Styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h2>Cotizador</h2>
+    <Header 
+      titulo="Cotizador de Seguros"
+    />
   );
 }
 
