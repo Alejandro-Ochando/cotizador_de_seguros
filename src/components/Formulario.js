da238e5bf0c57ba1006cb27abbfa8127b2311a74
Alejandro-Ochando <alejandro.ochando@live.com>
2020-09-28 19:55:39 +0200

    Leyendo la informacion del Usuario al seleccionar una opcion

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 const Campo = styled.div`
@@ -47,12 +47,37 @@ const Boton = styled.button`
 
 
 const Formulario = () => {
+    
+    const [ datos, guardarDatos ] = useState ({
+
+        marca: '',
+        year: '',
+        plan: ''
+    });
+    
+    //Extraer los valores del State
+    const { marca, year, plan } = datos;
+    
+    //Leer los datos del Formulario y colorcarlos en el State
+    const obtenerInformacion = e => {
+        guardarDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+
     return ( 
 
         <form>
             <Campo>
                 <Label>Marca</Label>
-                <Select>
+                <Select
+                    name="marca"
+                    value={marca}
+                    onChange={obtenerInformacion}
+                >
                     <option value="">-- Seleccione --</option>
                     <option value="Europeo">Europeo</option>
                     <option value="Americano">Americano</option>
@@ -61,7 +86,11 @@ const Formulario = () => {
             </Campo>
             <Campo>
                 <Label>Año</Label>
-                <Select>
+                <Select
+                    name="year"
+                    value={year}
+                    onChange={obtenerInformacion}
+                >
                 <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -81,11 +110,15 @@ const Formulario = () => {
                     type="radio"
                     name="plan"
                     value="basico"
+                    checked={plan === "basico"}
+                    onChange={obtenerInformacion}
                 />Básico
                 <InputRadio
                     type="radio"
                     name="plan"
                     value="completo"
+                    checked={plan === "completo"}
+                    onChange={obtenerInformacion}
                 />Completo
             </Campo>
             
